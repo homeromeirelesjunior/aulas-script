a82c4a22f20cc21e855c43ac87720e176e69a19b
Homero Meireles Junior <65316962+homeromeirelesjunior@users.noreply.github.com>
2021-08-12 10:57:10 -0300

    (12/08) "first commit"

---
+++ menu-horizontal/src/Components/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header id="corpo">
+        <nav id="menu">
+          <ul>
+            <li><a href="#inicio">Home</a></li>
+            <li><a href="#cursos">Cursos</a></li>
+            <li><a href="#contatos">Contatos</a></li>
+            <li><a href="#parceiros">Parceiros</a></li>
+            <li><a href="#missao">Missão</a></li>
+            <li><a href="#sobre">Sobre</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section id="inicio">
+        <h2>Inicio</h2>
+      </section>
+
+      <section id="cursos">
+        <h3>Cursos</h3>
+      </section>
+
+      <section id="contatos">
+        <h3>Contatos</h3>
+      </section>
+
+      <section id="parceiros">
+        <h3>Parceiros</h3>
+      </section>
+
+      <section id="missao">
+        <h3>Missão</h3>
+      </section>
+
+      <section id="sobre">
+        <h3>Sobre</h3>
+      </section>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
+/* Seções:
+1 - inicio/header
+2 - cursos
+3 - contatos
+4 - parceiros
+5 - missão
+6 - rodapé/footer
+*/
